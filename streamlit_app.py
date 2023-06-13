31241b91bb74f2d398b9dcfc33197d10b1ed312d
johnrjacksonsf <136361227+johnrjacksonsf@users.noreply.github.com>
2023-06-13 10:28:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
@@ -54,5 +50,8 @@ streamlit.dataframe(fruityvice_normalized)
 
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 #This will not work correctly, but just go with it for now
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
