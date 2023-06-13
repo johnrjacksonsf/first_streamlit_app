d882352f293e991bf2bd969b07771eb7bcf14ef2
johnrjacksonsf <136361227+johnrjacksonsf@users.noreply.github.com>
2023-06-13 10:16:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
+#This will not work correctly, but just go with it for now
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
